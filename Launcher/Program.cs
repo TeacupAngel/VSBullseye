afb4d9b6b88b463d1472ccb161835d445cf66a92
Rahjital <rahjital@seznam.cz>
2023-08-26 14:09:02 +0200

    Update to .NET 7

---
+++ Launcher/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Launcher
+{
+	public class LauncherEntry
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Launcher: Running Vintagestory.exe");
+
+			// Needed until this gets into production: https://github.com/dotnet/sdk/pull/30866
+			args = new string[] {
+				"--dataPath", Environment.GetEnvironmentVariable("VS_LAUNCH_DATAPATH"),
+				"--addModPath", Environment.GetEnvironmentVariable("VS_LAUNCH_MODPATH"),
+				"--playStyle", Environment.GetEnvironmentVariable("VS_LAUNCH_PLAYSTYLE"),
+				"--openWorld", Environment.GetEnvironmentVariable("VS_LAUNCH_WORLDNAME"),
+			};
+
+			VintagestoryClientWindows.ClientWindows.Main(args);
+		}
+	}
+}
---
+++ updateModTemplate.bat
@@ -0,0 +1,1 @@
+git --git-dir=.git-modtemplate --work-tree=. pull
